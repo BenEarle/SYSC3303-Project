14014451c018c4ab73720ca1f6d4978908406e57
shanecorrigan <shane.corrigan@outlook.com>
2017-05-03 16:53:04 -0400

    Update Server.java
    added user prompt to quit

--- src/Server.java
+++ src/Server.java
@@ -9,13 +9,22 @@ import util.Var;
 public class Server {
 	
 	public static void main(String[] args) throws SocketException {
-		Thread ct = new ControlThread();
+		ControlThread ct = new ControlThread();
 		ct.start();
 		boolean quit = false;
 		//Loop until the user types in Quit
 		while(!quit){
 			//get user input
 			//if user said quit quit = true
+			if (args.length > 0) {
+				int i;
+				for (i = 0; i < args.length; i++) {
+					if(args[i].equals("q") & args[i+1].equals("u") & args[i+1].equals("i") & args[i+1].equals("t")) {
+						quit = true;
+					}
+				}
+			}
+			if (quit) ct.close();
 		}
 	}
 
