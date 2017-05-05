08eac8b9ce45b55dcda5fb2aeba4208e406dc07c
pattyperron <patrick.perron.12@gmail.com>
2017-05-05 17:53:49 -0400

    Fixed missing functionality

--- src/ReadThread.java
+++ src/ReadThread.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 
 import util.FileReader;
-import util.FileWriter;
 import util.Log;
 import util.Var;
 
@@ -30,43 +29,95 @@ public class ReadThread extends ClientResponseThread {
 		try {
 			fr = new FileReader(file);
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			Log.err(e.getStackTrace().toString());
 		}
+		
+		// Initialize block index to send first block
 		byte[] blockNum = new byte[2];
-		blockNum[1] = 0x01;
 		blockNum[0] = 0x00;
-		byte[] data = null;
+		blockNum[1] = 0x01;
+		
+		boolean lastPacket = false; // flag to indicate if last packet is sent
+		byte[] data = null; // data from file
+		byte[] msg  = null; // bytes in message to send
+		
+		// Get data from file
 		try {
 			data = fr.read();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			// Check if length read is less than full block size
+			if(data.length != Var.BLOCK_SIZE) lastPacket = true; 
+		} catch (Exception e) {
+			data = new byte[1]; // Empty Message
+			lastPacket = true;
 		}
-		int dataLength = data.length;
-		super.sendPacket(data);
-		if(verbose) Log.out("SERVER<ReadThread>: Sending READ Data:" + data.toString());	
-		while (dataLength % Var.BLOCK_SIZE == 0) {
+
+		// Build message from data
+		msg = new byte[4+data.length];
+		msg[0] = Var.DATA[0];
+		msg[1] = Var.DATA[1];
+		msg[2] = blockNum[0];
+		msg[3] = blockNum[1];
+		for(int i=0; i<data.length; i++) msg[i+4] = data[i]; // Copy data into message
+		
+		// Send first data packet
+		if(verbose) Log.out("SERVER<ReadThread>: Sending Initial READ Data");	
+		super.sendPacket(msg);
+
+		// Loop until all packets are sent
+		while (!lastPacket) {
+			// Receive packet
+			if(verbose) Log.out("SERVER<ReadThread>: Receiving ACK Data");
 			packet = super.receivePacket();
-			if (packet.getData()[1] == Var.ACK[1]) {
+			// Ensure packet is ack
+			if (packet.getData()[0]==Var.ACK[0] &&  packet.getData()[1]==Var.ACK[1]) {
+				// Ensure packet has correct index
 				if (packet.getData()[2] == blockNum[0] && packet.getData()[3] == blockNum[1]) {
+					// Get data from file and check if length read is less than full block size 
 					try {
 						data = fr.read();
-					} catch (IOException e) {
-						e.printStackTrace();
+						if(data.length != Var.BLOCK_SIZE) lastPacket = true; 
+					// Exception if no bytes left in file. Send last packet empty
+					} catch(Exception e) {
+						data = new byte[1]; // Empty Message
+						lastPacket = true;
 					}
-					dataLength = data.length;
+					
+					// Increment Block Number
 					blockNum = bytesIncrement(blockNum);
-					super.sendPacket(data);
-					if(verbose) Log.packet("SERVER<ReadThread>: Sending READ Data", packet);
-				}
-			}
+					
+					// Build message from data
+					msg = new byte[4+data.length];
+					msg[0] = Var.DATA[0];
+					msg[1] = Var.DATA[1];
+					msg[2] = blockNum[0];
+					msg[3] = blockNum[1];
+					for(int i=0; i<data.length; i++) msg[i+4] = data[i]; // Copy data into message
+					
+					// Send Packet
+					if(verbose) Log.out("SERVER<ReadThread>: Sending READ Data");
+					super.sendPacket(msg);
+					
+				} else throw new IndexOutOfBoundsException();
+			} else throw new IllegalArgumentException();
 		}
+		// Receive final ACK packet
+		if(verbose) Log.out("SERVER<ReadThread>: Receiving Final ACK Data");
 		packet = super.receivePacket();
-		if (packet.getData()[1] == Var.ACK[1]) {
+		// Ensure ACK is valid
+		if (packet.getData()[0]==Var.ACK[0] && packet.getData()[1]==Var.ACK[1]) {
+			// Ensure block number is valid
 			if (packet.getData()[2] == blockNum[0] && packet.getData()[3] == blockNum[1] && verbose) {
+				
 				Log.out("SERVER<ReadThread>: Read completed successfully.");
-			}
+				
+			} else throw new IndexOutOfBoundsException();
+		} else throw new IllegalArgumentException();
+		
+		// Close file
+		try {
+			fr.close();
+		} catch (IOException e) {
+			Log.err(e.getStackTrace().toString());
 		}
 	}
 	
