e7fe959c29b4c7d322319d3359e4678bbff53919
Dillon Verhaeghe <nollid30@hotmail.com>
2017-05-10 10:37:01 -0400

    got rid of unneeded client imports :hear_no_evil:

--- src/Client.java
+++ src/Client.java
@@ -1,13 +1,10 @@
 
-//import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 import java.net.SocketException;
-import java.util.ArrayList;
-//import java.util.Arrays;
 import java.util.Scanner;
 
 import util.FileReader;
