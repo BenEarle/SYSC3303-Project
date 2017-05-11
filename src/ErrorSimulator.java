13b17f5088085082208af692a3ac984ef3f6545d
pattyperron <patrick.perron.12@gmail.com>
2017-05-10 21:07:13 -0400

    Format Fixing

--- src/ErrorSimulator.java
+++ src/ErrorSimulator.java
@@ -22,19 +22,64 @@ public class ErrorSimulator {
 	private static final SocketAddress SERVER_ADDRESS = new InetSocketAddress("localhost", Var.PORT_SERVER);;
 	private boolean running;
 
-	private ErrorScenario errorScenario;
+	private ErrorScenario err;
 	
 	public ErrorSimulator() throws SocketException {
 		socket = new DatagramSocket(Var.PORT_CLIENT);
-		errorScenario = new ErrorScenario();
+		err    = new ErrorScenario();
 	}
-	
 	
 	private void display(DatagramPacket packet){
 		for(int i=0; i<packet.getData().length; i++){
 			System.out.print(packet.getData()[i]+", ");
 		}System.out.print("\n");
 	}
+	
+	/*************************************************************************/
+	// This method checks a packet for existing conditions comparing to the
+	// error scenario. If the correct scenario is present, the sabotage 
+	// method is called on the packet, else it is unchanged
+	/*************************************************************************/
+	
+	private DatagramPacket checkSabotage(DatagramPacket packet){
+		byte[] data = packet.getData();
+		if       ( data[1]==Var.READ[1] && err.getPacketType()==ErrorScenario.READ_PACKET){
+			packet = err.Sabotage(packet);
+			Log.out(
+				"ErrorSimulatorChannel: READ Packet sabotaged with "+err.getFaultType()
+				+" (CODE: "+err.getErrorCode()+")"
+			);
+			return packet;
+		} 
+		if( data[1]==Var.WRITE[1]  && err.getPacketType()==ErrorScenario.WRITE_PACKET){
+			packet = err.Sabotage(packet);
+			Log.out(
+				"ErrorSimulatorChannel: WRITE Packet sabotaged with "+err.getFaultType()
+				+" (CODE: "+err.getErrorCode()+")"
+			);
+			return packet;
+		}
+		if( data[1] == Var.DATA[1] && err.getPacketType()==ErrorScenario.DATA_PACKET 
+										  && (data[2]*256+data[3]) == err.getBlockNum()){
+			packet = err.Sabotage(packet);
+			Log.out(
+				"ErrorSimulatorChannel: DATA Packet #"+err.getBlockNum()+"sabotaged with "+err.getFaultType()
+				+" (CODE: "+err.getErrorCode()+")"
+			);
+			return packet;
+		} 
+		if( data[1] == Var.ACK[1]  && err.getPacketType()==ErrorScenario.ACK_PACKET  
+									 	  && (data[2]*256+data[3]) == err.getBlockNum()){
+			packet = err.Sabotage(packet);
+			Log.out(
+				"ErrorSimulatorChannel: ACK Packet #"+err.getBlockNum()+"sabotaged with "+err.getFaultType()
+				+" (CODE: "+err.getErrorCode()+")"
+			);
+			return packet;
+		}
+		return packet;
+	}
+	
 	
 	/*************************************************************************/
 	// This method creates and runs a temporary channel between server and 
@@ -49,63 +94,52 @@ public class ErrorSimulator {
 
 		// Set up variables to remember addresses of client and server
 		SocketAddress addrClient = null;
-		SocketAddress addrServer = new InetSocketAddress("localhost", Var.PORT_SERVER);; // after first response from server
-
+		SocketAddress addrServer = SERVER_ADDRESS;//new InetSocketAddress("localhost", Var.PORT_SERVER);
+		
 		// Packet used to transfer between client and server
 		DatagramPacket packet = new DatagramPacket(new byte[Var.BUF_SIZE], Var.BUF_SIZE);
 		
-		// data used
+		// Data and Flags used in transfer
 		byte[] data;
-		boolean lastData = false;
-		boolean lastAck  = false;
-		boolean started  = false;
+		boolean lastData  = false;
+		boolean lastAck   = false;
+		boolean error     = false;
+		boolean initiated = false;
 		
 		
-		// Begin loop: forward message to client, get response and forward to server ...
+		// Begin loop: Get Response from Client and forward to server, vice-versa
 		running = true;
 		while (running) {
-
 			// Receive packet from Client
-			if(!started) socket.receive(packet);
-			else      socClient.receive(packet);
+			if(!initiated) socket.receive(packet); // Receive on Well-Known port socket
+			else        socClient.receive(packet); // Receive on Channel-Dedicated port socket
 			
-			started = true;
+			initiated = true; // If here transfer has been initiated
 			
+			// Display packet that was received
 			Log.packet("ErrorSimulatorChannel: Client -> ErrorSim", packet);
 			display(packet);
 			
 			// Update address for Client, where future requests are sent
 			addrClient = packet.getSocketAddress();
 
-			//**************************************
 			// Parse Packet from Client
 			data = packet.getData();
-			//Check for last DATA packet
+			//Check if last DATA packet
 			if (data[0] == Var.DATA[0] && data[1] == Var.DATA[1] && packet.getLength() != 516) {
-				Log.out("Received Last Packet");
+				Log.out("ErrorSimulatorChannel: Received Last DATA Packet in Transfer");
 				lastData = true;
 			//Check for last ACK packet
 			} else if (data[0] == Var.ACK[0] && data[1] == Var.ACK[1] && lastData == true) {
+				Log.out("ErrorSimulatorChannel: Received Last ACK Packet in Transfer");
 				lastAck = true;
 			}
 			// Check whether or not to sabotage
-			if( data[1] == Var.READ[1] && errorScenario.packetType == errorScenario.READ_PACKET){
-				packet = errorScenario.Sabotage(packet);
-				Log.out("ErrorSimulator: READ Packet sabotaged");
+			packet = checkSabotage(packet);
+			//Check for Error packet
+			if (data[0] == Var.ERROR[0] && data[1] == Var.ERROR[1]) {
+				error = true;
 			}
-			if( data[1] == Var.WRITE[1] && errorScenario.packetType == errorScenario.WRITE_PACKET){
-				packet = errorScenario.Sabotage(packet);
-				Log.out("ErrorSimulator: WRITE Packet sabotaged");
-			}
-			if( data[1] == Var.DATA[1] && errorScenario.packetType == errorScenario.DATA_PACKET && (data[2]*256+data[3]) == errorScenario.blockNum){
-				packet = errorScenario.Sabotage(packet);
-				Log.out("ErrorSimulator: DATA Packet sabotaged");
-			}
-			if( data[1] == Var.ACK[1] && errorScenario.packetType == errorScenario.ACK_PACKET && (data[2]*256+data[3]) == errorScenario.blockNum){
-				packet = errorScenario.Sabotage(packet);
-				Log.out("ErrorSimulator: ACK Packet sabotaged");
-			}
-			//**************************************
 			
 			// Send socket to Server
 			packet.setSocketAddress(addrServer);
@@ -113,10 +147,11 @@ public class ErrorSimulator {
 			Log.packet("ErrorSimulatorChannel: ErrorSim -> Server", packet);
 			display(packet);
 
-			// Break if lastAck was sent
-			if (lastData && lastAck)
-				break;
-
+			// Quit if lastAck was sent
+			if ((lastData && lastAck) || error) break;
+			
+			//-----------------------------------------
+			
 			// Receive back from Server
 			socServer.receive(packet);
 			Log.packet("ErrorSimulatorChannel: Server -> ErrorSim", packet);
@@ -125,46 +160,32 @@ public class ErrorSimulator {
 			// Update address for server, where future requests are sent
 			addrServer = packet.getSocketAddress();
 			
-			//**************************************
 			// Parse Packet from Server
 			data = packet.getData();
-			//Check for last DATA packet
+			//Check if last DATA packet
 			if (data[0] == Var.DATA[0] && data[1] == Var.DATA[1] && packet.getLength() != 516) {
-				Log.out("Received Last Packet");
+				Log.out("ErrorSimulatorChannel: Received Last DATA Packet in Transfer");
 				lastData = true;
 			//Check for last ACK packet
 			} else if (data[0] == Var.ACK[0] && data[1] == Var.ACK[1] && lastData == true) {
+				Log.out("ErrorSimulatorChannel: Received Last ACK Packet in Transfer");
 				lastAck = true;
 			}
 			// Check whether or not to sabotage
-			if( data[1] == Var.READ[1] && errorScenario.errorCode == errorScenario.READ_PACKET){
-				packet = errorScenario.Sabotage(packet);
-				Log.out("ErrorSimulator: READ Packet sabotaged");
+			packet = checkSabotage(packet);
+			//Check for Error packet
+			if (data[0] == Var.ERROR[0] && data[1] == Var.ERROR[1]) {
+				error = true;
 			}
-			if( data[1] == Var.WRITE[1] && errorScenario.errorCode == errorScenario.WRITE_PACKET){
-				packet = errorScenario.Sabotage(packet);
-				Log.out("ErrorSimulator: WRITE Packet sabotaged");
-			}
-			if( data[1] == Var.DATA[1] && errorScenario.errorCode == errorScenario.DATA_PACKET && (data[2]*256+data[3]) == errorScenario.blockNum){
-				packet = errorScenario.Sabotage(packet);
-				Log.out("ErrorSimulator: DATA Packet sabotaged");
-			}
-			if( data[1] == Var.ACK[1] && errorScenario.errorCode == errorScenario.ACK_PACKET && (data[2]*256+data[3]) == errorScenario.blockNum){
-				packet = errorScenario.Sabotage(packet);
-				Log.out("ErrorSimulator: ACK Packet sabotaged");
-			}
-			//**************************************
-			
+						
 			// Send socket to Client
 			packet.setSocketAddress(addrClient);
 			socClient.send(packet);
 			Log.packet("ErrorSimulatorChannel: ErrorSim -> Client", packet);
 			display(packet);
 			
-
-			// Break if lastAck was sent
-			if (lastData && lastAck)
-				break;
+			// Quit if lastAck was sent
+			if ((lastData && lastAck) || error) break;
 		}
 		socClient.close();
 		socServer.close();
@@ -172,14 +193,9 @@ public class ErrorSimulator {
 	}
 
 	public void run() throws IOException {
-
 		running = true;
-
 		while (running) {
-
-			// Create and run a channel for received request
 			runChannel();
-			
 		}
 	}
 
@@ -197,72 +213,4 @@ public class ErrorSimulator {
 	public static void main(String[] args) throws SocketException, IOException {
 		new ErrorSimulator().run();
 	}
-
 }
-
-
-/*
-// Receive request from client
-socket.receive(requestPacket);
-Log.packet("ErrorSimulator: Client -> ErrorSim", requestPacket);
-for(int i=0; i<requestPacket.getData().length; i++){
-	System.out.print(requestPacket.getData()[i]+", ");
-}System.out.print("\n");
-
-//**************************************
-// Parse Packet from Client
-data = requestPacket.getData();
-// Check block number and type to determine wether or not to sabotage
-if( data[1] == errorScenario.errorCode ) {
-	requestPacket = errorScenario.Sabotage(requestPacket);
-	Log.out("ErrorSimulator: READ/WRITE Request Packet Sabotaged");
-}
-//**************************************
-
-
-// First packet goes to Server's well-known port
-requestPacket.setSocketAddress(SERVER_ADDRESS);
-socServer.send(requestPacket);
-Log.packet("ErrorSimulatorChannel: Server -> ErrorSim", requestPacket);
-for(int i=0; i<requestPacket.getData().length; i++){
-	System.out.print(requestPacket.getData()[i]+", ");
-}System.out.print("\n");
-
-
-// Receive back from server
-DatagramPacket packet = new DatagramPacket(new byte[Var.BUF_SIZE], Var.BUF_SIZE);
-socServer.receive(packet);
-Log.packet("ErrorSimulatorChannel: Server -> ErrorSim", packet);
-for(int i=0; i<packet.getData().length; i++){
-	System.out.print(packet.getData()[i]+", ");
-}System.out.print("\n");
-
-// Update address for server, where future requests are sent
-addrServer = packet.getSocketAddress();
-
-//**************************************
-// Parse Packet from Server
-data = packet.getData();
-// Check for last data packet
-if (data[0] == Var.DATA[0] && data[1] == Var.DATA[1] && packet.getLength() != 516) {
-	Log.out("Received Last Packet");
-	lastData = true;
-// Check for last ack packet
-} else if (data[0] == Var.ACK[0] && data[1] == Var.ACK[1] && lastData == true) {
-	lastAck = true;
-}
-// Check block number and type to determine wether or not to sabotage
-if( data[1] == errorScenario.errorCode && (data[2]*256+data[3]) == errorScenario.blockNum ) {
-	packet = errorScenario.Sabotage(packet);
-	Log.out("ErrorSimulator: READ/WRITE Packet sabotaged");
-}
-//**************************************
-
-// Send first packet back to client
-packet.setSocketAddress(addrClient);
-socClient.send(packet);
-Log.packet("ErrorSimulatorChannel: ErrorSim -> Client", packet);
-for(int i=0; i<packet.getData().length; i++){
-	System.out.print(packet.getData()[i]+", ");
-}System.out.print("\n");
- */
